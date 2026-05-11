311b938842c474d0a3786acfccdf4677517fcce0
jayabalajeeskwork <work.jayabalajeesk@gmail.com>
2026-05-11 18:20:51 +0530

    Check

---
+++ JAVA/Hello.java
@@ -0,0 +1,19 @@
+package JAVA;
+
+public class Hello {
+
+    public void print(int a) {
+
+    }
+
+    public static void main(String[] args) {
+
+        int i = 10;
+        do {
+            System.out.println(i);
+            System.out.println("Inside Loop");
+            i--;
+        } while (i >= 0);
+        System.out.println("Out side loop");
+    }
+}
