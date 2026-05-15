1c99ddd502ff51698125dd48163a1b21d7a83cdb
Jayabalajee <jayabalajeesk04@gmail.com>
2026-05-15 23:05:02 +0530

    eqrw

---
+++ JAVA1/SMSJavaProject/sms-project-backend/src/test/java/com/jb/sms_project_backend/SmsProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jb.sms_project_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
