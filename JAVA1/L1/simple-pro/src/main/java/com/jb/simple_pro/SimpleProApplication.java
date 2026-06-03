e25f4263d3115fbb79032fba84b698141366b0bd
Jayabalajee <jayabalajeesk04@gmail.com>
2026-06-03 21:44:16 +0530

    Simple Pro - Java

---
+++ JAVA1/L1/simple-pro/src/main/java/com/jb/simple_pro/SimpleProApplication.java
@@ -0,0 +1,13 @@
+package com.jb.simple_pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleProApplication.class, args);
+	}
+
+}
---
+++ JAVA1/L1/simple-pro/src/main/java/com/jb/simple_pro/UserController.java
@@ -0,0 +1,55 @@
+package com.jb.simple_pro;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class UserController {
+
+    @Autowired
+    private UserRepository repo;
+
+    @GetMapping("/")
+    public String registerPage() {
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String register(User user) {
+
+        repo.save(user);
+
+        return "redirect:/login";
+    }
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String login(
+            @RequestParam String email,
+            @RequestParam String password,
+            Model model) {
+
+        User user =
+                repo.findByEmailAndPassword(
+                        email,
+                        password);
+
+        if(user != null) {
+
+            model.addAttribute(
+                    "username",
+                    user.getUsername());
+
+            return "dashboard";
+        }
+
+        return "login";
+    }
+}
