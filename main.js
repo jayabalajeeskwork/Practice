e16330449758c9d3d7b9807758c7beb0a36d61bd
jayabalajeeskwork <work.jayabalajeesk@gmail.com>
2026-08-05 18:58:16 +0530

    js

---
+++ main.js
@@ -0,0 +1,27 @@
+let a = 10
+let b = 20
+a+=10 //a = a+10
+console.log(a==b)
+age = 16
+let res = (age>=18)? "Eligible" : "Not Eligible"
+console.log(res)
+let c = 10
+console.log(c++)
+console.log(++c)
+console.log(c--)
+console.log(--c)
+console.log(c)
+
+let d = 10
+d = d++ + ++d
+console.log(d)
+console.log()
+console.log(10 == 10) // true
+console.log("10" == 10) // true
+console.log(10 != 10) // false
+console.log("10" != 10) //false
+console.log()
+console.log(10 === 10) // true
+console.log("10" === 10) // false
+console.log(10 !== 10) // false
+console.log("10" !== 10) //true
